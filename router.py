dd292fa23a7d487472786ed6f13bedc3ad64e514
Yiliang Xiong <wlxiong@gmail.com>
2012-07-22 23:26:57 +0800

    return consistent results for one-to-one and one-to-many shortest paths

--- router.py
+++ router.py
@@ -6,7 +6,7 @@ from utils import logger, constant_factory
 from pprint import pprint
 
 
-def get_all_paths(net, start, prev, time):
+def get_all_shortest_paths(start, prev, time):
     # create a nested function for recursion
     def get_paths(node, path):
         # initialize a path set
@@ -25,9 +25,25 @@ def get_all_paths(net, start, prev, time):
         return paths
     return get_paths(start, [])
 
+
+def get_shortest_path(start, end, prev, time):
+    path = []
+    # start from the end node
+    path_head = end.id
+    # go backward until arrive at the start node
+    while path_head != start.id:
+        # agument the path with a edge and arrival time
+        prev_edge = prev[path_head]
+        path_head = prev_edge.head.id
+        path.insert(0, (prev_edge, time[path_head]))
+    return path
+
+
 def find_shortest_path(net, depart_time, start, end=None):
     if end != None and start == end:
-        return {'path': None, 'cost': 0.0, 'time': 0.0}
+        return {'path': {end.id: None}, 
+                'cost': {end.id: 0.0}, 
+                'time': {end.id: 0.0}}
     # creater a FIFO queue for searching
     queue = deque()
     # create containers with default values
@@ -56,23 +72,16 @@ def find_shortest_path(net, depart_time, start, end=None):
                 prev[edge.tail.id] = edge
                 # and append the expanded node to the queue
                 queue.appendleft(edge.tail.id)
-    # if end node is given, extract the shortest path recursively
+    # if end node is given, extract the shortest path to end node recursively
     if end != None:
-        # if the end node is reached, at least
-        # there is one path between the start and end nodes
-        if end.id in prev:
-            # initialize the path with the last edge on it
-            path = [(prev[end.id], time[prev[end.id].head.id])]
-            # go backward until arrive at the start node
-            while path[0][0].head.id != start.id:
-                # agument the path with a edge and arrival time
-                path.insert(0, (prev[path[0][0].head.id], time[prev[path[0][0].head.id].head.id]))
-            return {'path': path, 'cost': cost[end.id], 'time': time[end.id]}
-        else:
-            # if the end node is not reached, the start and end nodes are not connected
-            return {'path': None, 'cost': float('+inf'), 'time': float('+inf')}
+        # if the end node is reached, there is at least one path
+        # if the end node is not reached, the start and end nodes are not connected
+        path = get_shortest_path(start, end, prev, time) if end.id in prev else None
+        return {'path': {end.id: path}, 
+                'cost': {end.id: cost[end.id]}, 
+                'time': {end.id: time[end.id]}}
     else:
         # if end node is not given, extract the shortest paths from start to all the other nodes
-        all_paths = get_all_paths(net, start, prev, time)
-        ends = [path[-1][0].tail.id for path in all_paths]
-        return dict(zip(ends,all_paths))
+        all_paths = get_all_shortest_paths(start, prev, time)
+        end_ids = [path[-1][0].tail.id for path in all_paths]
+        return {'path': dict(zip(end_ids, all_paths)), 'cost': cost, 'time': time}
