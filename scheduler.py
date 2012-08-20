5b5971e083c4f238d40758cb0cd013efb4b6c13d
Yiliang Xiong <wlxiong@gmail.com>
2012-08-21 00:54:47 +0800

    reconstruct the state and transition generators

--- scheduler.py
+++ scheduler.py
@@ -10,48 +10,49 @@ from utils import logger
 State = namedtuple('State', 'tick, activity, location, elapsed, joint, todo')
 
 
-def traverse_indvidual_states(person, locations):
+def activity_locations(person, activity, land):
+    if activity.name == "work" or activity.name == "school":
+        return [person.get_workplace()]
+    elif activity.name == "home":
+        return [person.get_residence()]
+    else:
+        return land.get_locations(activity.name)
+
+
+def individual_states(person, land):
     for tick in xrange(Time.MAXTICK-1,-1,-1):
         for activity in person.program:
-            if not activity.within_time_window(tick):
-                continue
-            if activity.name == "work" or activity.name == "school":
-                activity_locations = [person.get_workplace()]
-            elif activity.name == "home":
-                activity_locations = [person.get_residence()]
-            else:
-                activity_locations = locations[activity.name]
-            for location in activity_locations:
-                for elapsed in xrange(tick):
-                    if activity.within_time_window(tick - elapsed):
-                        yield (tick, activity, location, elapsed, None, None)
+            if activity.within_time_window(tick):
+                for location in activity_locations(person, activity, land):
+                    for elapsed in xrange(tick):
+                        if activity.within_time_window(tick - elapsed):
+                            yield (tick, activity, location, elapsed, None, None)
 
-def traverse_indvidual_transitions(person, locations, network, router,
-                                   tick, activity, position, elapsed, joint, todo):
+
+def individual_transitions(person, land, network, router,
+                           tick, activity, position, elapsed, joint, todo):
     for activity in person.program:
-        if activity.name == "work" or activity.name == "school":
-            activity_locations = [person.get_workplace()]
-        elif activity.name == "home":
-            activity_locations = [person.get_residence()]
-        else:
-            activity_locations = locations[activity.name]
-        for destination in activity_locations:
-            path, cost, time = router.get_shortest_path(tick, position.id, destination.id)
-            yield (activity, destination, path, cost, time)
-        
+        for destination in activity_locations(person, activity, land):
+            path, tcost, ttime = router.get_shortest_path(tick, position.id, destination.id)
+            yield (activity, destination, path, tcost, ttime)
+
 
 def individual_schedule(pop, net, land, router, demand):
-    locations = land.get_locations()
-    for hh in pop.households[1:100]:
-        logger.debug(hh)
+    for hh in pop.households[:1000]:
+        if hh.id % 200 == 0:
+            print " .%d" % hh.id
+        # logger.debug((hh, hh.program))
         for person in itertools.chain(hh.adults, hh.children):
-            logger.debug(person)
-            for state in traverse_indvidual_states(person, locations):
+            # logger.debug((person, person.program))
+            for state in individual_states(person, land):
                 tick, activity, position, elapsed, joint, todo = state
-                logger.debug(state)
-                for trans in traverse_indvidual_transitions(person, locations, net, router, *state):
+                # logger.debug(state)
+                for trans in individual_transitions(person, land, net, router, *state):
                     next_activity, destination, path, travel_cost, travel_time = trans
-                    logger.debug(trans)
+                    # logger.debug(trans)
+                    # arrival time at next activity destination
+                    arrival_time = tick + travel_time + 1
+                    
                     
     print "individual_schedule()"
         
